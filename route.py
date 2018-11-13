3e5273a38ec639cb8cf4a6d3072ac1e8cf5ea485
fyfirst <543509056@qq.com>
2018-11-13 21:32:14 +0800

    merge finshed

--- route.py
+++ route.py
@@ -16,7 +16,11 @@ route_handler = [
     (r"/confirm_score", handler.ConfirmSCHandler),
     (r"/cookie", handler.CookieHandler),
     (r"/test", handler.TestqueryHandler),
+<<<<<<< HEAD
+    (r"/updatePassword",handler.ModifyPasswordHandler),
+=======
     (r"/modify_password",handler.ModifyPasswordHandler),
+>>>>>>> b83a714d993dcc29ba092ab774144004fe93935b
 
     (r"/get_insert_data", handler.InsertDataHandler),
     (r"/get_table_data",handler.TableDataHandle),
@@ -25,6 +29,15 @@ route_handler = [
     (r"/getScoreData",handler.GetScoreDataHandler),
     (r"/getAllScore",handler.getAllScoreHandler),
 
+<<<<<<< HEAD
+	(r"/get_GPA",handler.GetUserGPAHandler),
+    (r"/get_Course",handler.GetUserCourseHandler),
+    (r"/get_GPATime",handler.GetUserGPATimeHandler),
+    (r"/get_ChosenGPA",handler.GetUserChosenGPAHandler),
+    (r"/get_Score",handler.GetUserCourseScoreHandler)
+
+=======
+>>>>>>> b83a714d993dcc29ba092ab774144004fe93935b
 
 ]
 
